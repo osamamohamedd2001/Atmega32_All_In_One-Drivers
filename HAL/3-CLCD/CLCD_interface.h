6cc335a70a170d41a3c443614b05c77d59ea53a1
osama_mohamed <osamamohamedd2001@gmail.com>
2024-07-16 18:55:59 +0200

    Adding new CLCD Module files

--- HAL/3-CLCD/CLCD_interface.h
+++ HAL/3-CLCD/CLCD_interface.h
@@ -7,17 +7,16 @@ void CLCD_voidSendData(uint8 Copy_u8Data);
 
 void CLCD_voidInit(void);
 
-void CLCD_voidSendNumber(sint32 Copy_s32Number);
+void CLCD_u8ClearDisplay(void);
 
 uint8 CLCD_u8SendString(const char* Copy_pchString);
 
-void CLCD_voidSetCursorToXY(uint8 Copy_u8XPos, uint8 Copy_u8YPos);
+void CLCD_voidSendNumber(sint32 Copy_s32Number);
 
-uint8 CLCD_u8DisplaySpecialCharacter(uint8 Copy_u8LocationNum, uint8 *Copy_pu8Pattern, uint8 Copy_u8XPos, uint8 Copy_u8YPos);
+void CLCD_voidGoToXY(uint8 Copy_u8XPos, uint8 Copy_u8YPos);
 
-void CLCD_u8ClearDisplay(void);
+uint8 CLCD_u8SendSpecialCharacter(uint8 Copy_u8LocationNum, uint8* Copy_pu8Pattern, uint8 Copy_u8XPos, uint8 Copy_u8YPos);
 
-void CLCD_voidEnableBlinkingCursor(uint8 Copy_u8State);
 
 
 #endif
